a6b48b123fc6dc1d6e2fe0fc3b846ef747c246d7
matt_carmody <matt.carmody@ovsgroup.com>
2023-06-25 10:04:18 -0500

    add files with instructions

---
+++ Lab6_Part1.py
@@ -0,0 +1,13 @@
+# Matt Carmody
+# COSC 6375
+# Lab 6 Part 1
+
+"""
+INSTRUCTIONS:
+Part 1. Duplicate Word Removal
+This is a problem in the textbook, Problem 6.6, on page 235.
+Write a Python script in a file named Lab6_Part1.py.
+Write a function that receives a list of words, then determines and displays in alphabetical order only the
+unique words. Treat uppercase and lowercase letters the same. The function should use a set to get the
+unique words in the list. Test your function.
+"""
---
+++ Lab6_Part2.py
@@ -0,0 +1,14 @@
+# Matt Carmody
+# COSC 6375
+# Lab 6 Part 2
+
+"""
+INSTRUCTIONS:
+Part 2. Character Counts
+This is a problem in the textbook, Problem 6.7, on page 235.
+Write a Python script in a file named Lab6_Part2.py.
+Recall that strings are sequences of characters. Use techniques similar to Fig. 6.2 to write a script that inputs
+a sentence from the user, then uses a dictionary to summarize the number of occurrences of each letter.
+Ignore case, ignore blanks and assume the user does not enter any punctuation. Display a two-column table
+of the letters and their counts with the letters in sorted order.
+"""
