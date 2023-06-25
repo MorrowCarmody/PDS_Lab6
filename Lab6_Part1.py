55a22c0becff256bc430861a8f0b09e4d85b824e
matt_carmody <matt.carmody@ovsgroup.com>
2023-06-25 15:19:52 -0500

    complete working version of function

--- Lab6_Part1.py
+++ Lab6_Part1.py
@@ -3,10 +3,16 @@
 # Lab 6 Part 1
 
 def word_sort(words):
-    words = words.lower()
-    return
+    word_set = set()
+    
+    # add items to set to remove duplicates
+    for word in words:
+        word_set.add(word.lower())
 
-word_sort('This is a test sentence written by Matt')
+    # return sorted list
+    return list(sorted(word_set))
+
+print(word_sort(['This','is','a','test','sentence','written','by','Matt','to','test','the','funciton']))
 
 """
 INSTRUCTIONS:
