99c541546fa4cf2b76b7592da1ff0ff50c913401
MorrowCarmody <morrow.carmody@gmail.com>
2023-07-12 13:59:00 -0500

    remove instructions

--- Lab6_Part1.py
+++ Lab6_Part1.py
@@ -14,13 +14,3 @@ def word_sort(words):
     return list(sorted(word_set))
 
 print(word_sort(['This','is','a','test','sentence','written','by','Matt','to','test','the','funciton']))
-
-"""
-INSTRUCTIONS:
-Part 1. Duplicate Word Removal
-This is a problem in the textbook, Problem 6.6, on page 235.
-Write a Python script in a file named Lab6_Part1.py.
-Write a function that receives a list of words, then determines and displays in alphabetical order only the
-unique words. Treat uppercase and lowercase letters the same. The function should use a set to get the
-unique words in the list. Test your function.
-"""
