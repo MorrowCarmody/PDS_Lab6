5221f25bde0da19666619ab982d09ba973fa992a
MorrowCarmody <morrow.carmody@gmail.com>
2023-07-12 13:59:19 -0500

    remove instructions

--- Lab6_Part2.py
+++ Lab6_Part2.py
@@ -15,14 +15,3 @@ def char_count(sentence):
     return
 
 char_count('This is a test sentence written by Matt')
-
-"""
-INSTRUCTIONS:
-Part 2. Character Counts
-This is a problem in the textbook, Problem 6.7, on page 235.
-Write a Python script in a file named Lab6_Part2.py.
-Recall that strings are sequences of characters. Use techniques similar to Fig. 6.2 to write a script that inputs
-a sentence from the user, then uses a dictionary to summarize the number of occurrences of each letter.
-Ignore case, ignore blanks and assume the user does not enter any punctuation. Display a two-column table
-of the letters and their counts with the letters in sorted order.
-"""
